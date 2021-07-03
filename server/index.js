25dd11899505fdb50115c860133a7b8ab4728d52
SaW2017 <s1wachter@edu.aau.at>
2021-07-03 16:22:26 +0200

    adding libraries

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 // Setting up an express-server
-const express = require('express');
-
-// importing mongoose library
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors")
 const app = express();
 
 const ConceptModel = require("./models/ Concept");
 
+// parsing JSON which is coming from the Frontend
 app.use(express.json());
+app.use(cors());
 
 // String for connecting to mongoDB and passing an object
 mongoose.connect("mongodb+srv://adminuser:deeplearning1234@deep-learning.5fdov.mongodb.net/videosearchDB?retryWrites=true&w=majority", {
@@ -16,12 +17,15 @@ mongoose.connect("mongodb+srv://adminuser:deeplearning1234@deep-learning.5fdov.m
 
 // create a Route - if accessing this route,
 // we simply add something to the DB
-app.get('/', async (require, response) => {
+app.post('/insert', async (require, response) => {
 
-    // to test create variable
+    const conceptId = require.body.conceptId;
+    const conceptName = require.body.conceptName;
+
+    // to test inserting in DB
     const concept = new ConceptModel({
-        conceptId: 1,
-        name: "Airplane"
+        conceptId: conceptId,
+        name: conceptName
     });
 
     // try to save this constant into the DB-Collection
