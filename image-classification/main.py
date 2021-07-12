8dd8873652e945d65df7d64776b97a983e8557cc
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-07-12 10:48:07 +0200

    Added (non-functioning) main.py class

---
+++ image-classification/main.py
@@ -0,0 +1,14 @@
+import shot_detection
+import utils
+
+if __name__ == '__main__':
+    # print('start keyframe detection')
+    # st = shot_detection.ShotDetection()
+    #
+    # detected_keyframes = st.get_keyframes('everest.mp4')
+    # utils.write_images_to_disk(detected_keyframes)
+    #
+    # print('finished keyframe detection')
+
+    # print(utils.get_folder_elements_paths())
+    pass
