7d644abc9c604234738f867f945e3b48bee0deb5
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-07-21 23:49:03 +0200

    Refactored keyframe.py, now the actual keyframe_id is stored, and the final mongo representation was added.

--- image-classification/keyframe.py
+++ image-classification/keyframe.py
@@ -1,13 +1,9 @@
 from typing import Tuple, List
 
-from utils import get_opencv_histogram_bin
+import utils
 import json
 import numpy as np
 from mongoengine import Document, fields
-
-# used to convert np.ndarray to mongo_db record
-from bson.binary import Binary
-import pickle
 
 
 class NumpyEncoder(json.JSONEncoder):
@@ -20,44 +16,46 @@ class NumpyEncoder(json.JSONEncoder):
 class KeyframeDocuments(Document):
     keyframe_id: str = fields.StringField(required=True)
     file_path: str = fields.StringField(required=True)
-    concept_name: str = fields.StringField(required=True)
+    classifier: str = fields.StringField(required=True)
     concept_confidence: List[Tuple[str, int]] = fields.ListField(required=True)
-    # Image with size, False = dont force the size of the image
-    keyframe = fields.ListField(required=True)
-    # keyframe = fields.ImageField(thumbnail_size=(500, 450, False))
 
 
 class KeyFrameData(object):
 
     def __init__(self, keyframe: np.ndarray = None, file_path: str = None, index: int = 0):
-        self.keyframe: np.ndarray = keyframe
-        self.file_path = file_path
-        self.index = index
-        self.keyframe_id = f'{self.file_path}_{str(self.index)}'
-        self.histogram_bin = get_opencv_histogram_bin(self.keyframe)
-        self.classifier = 'resnet'
+        self.image: np.ndarray = keyframe
+        self.folder_path: str = self.__trunc_file_path(file_path)
+        self.index: int = index
+        self.keyframe_id: str = None
+        self.histogram_bin = utils.get_opencv_histogram_bin(self.image)
+        self.classifier: str = 'resnet'
         self.concepts: List[Tuple] = []
 
     def get_mongo_representation(self) -> KeyframeDocuments:
+        self.keyframe_id = f'{str(self.index)}.png'
         db_keyframe: KeyframeDocuments = KeyframeDocuments(
-            keyframe_id=f'{self.file_path}_image_{str(self.index)}',
-            file_path=self.file_path,
-            concept_name=self.classifier,
+            keyframe_id=f'{self.keyframe_id}',
+            file_path=self.folder_path,
+            classifier=self.classifier,
             concept_confidence=self.concepts,
-            # keyframe=self.keyframe.tolist()
-            keyframe=self.__convert_keyframe_to_mongo_record()
         )
-
         return db_keyframe
 
-    def __convert_keyframe_to_mongo_record(self) -> Binary:
-        # Source: https://stackoverflow.com/questions/6367589/saving-numpy-array-in-mongodb
-        record = Binary(pickle.dumps(self.keyframe, protocol=2), subtype=128)
-        return record
+    def __trunc_file_path(self, file_path: str):
+        if file_path is None:
+            return ''
+        temp_paths: list = file_path.split('/')
+        trunc_path: str = temp_paths.pop()
+        trunc_path = trunc_path.split('.')[0]
+
+        return trunc_path
 
     def __str__(self):
         return f'''
-        KeyFrameData {self.file_path}_{str(self.index)}
+        KeyFrameData
+            - file_path: {self.folder_path}
+            - index: {self.index}
+            - keyframe_id: {self.keyframe_id}
             - classifier: {self.classifier}
             - concepts: {self.concepts[:5]}
                     '''
