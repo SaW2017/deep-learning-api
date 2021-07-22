ff2509904a6da486db9aaf1e161ebbe9e3e759af
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-07-22 21:59:02 +0200

    Images are now stored in sub folders of public/images on the disk

--- image-classification/main.py
+++ image-classification/main.py
@@ -14,21 +14,17 @@ FRONT_END_PATH: str = '../../deep-learning-frontend/'
 IMAGES_PUBLIC_PATH: str = 'public/images/'
 
 if __name__ == '__main__':
-    print('start keyframe detection')
     st = shot_detection.ShotDetection()
-    cc = ConceptClassifier()
-
-    print('finished keyframe detection')
-
     folder_path_list: List[str] = FileWriter.get_folder_elements_paths()
     video_dict: Dict[str, List[KeyFrameData]] = {}
 
-    # print(FileWriter.get_folder_elements_paths('../../deep-learning-frontend/public'))
-
+    # Create root folder for images
     FileWriter.clean_create_directory(FRONT_END_PATH, IMAGES_PUBLIC_PATH)
 
+    # Used for sub folders of image root folders
     frontend_image_path: str = os.path.join(FRONT_END_PATH, IMAGES_PUBLIC_PATH)
 
+    print('start keyframe detection')
     # get the keyframes for each video and store them
     # also creating folder for images
     for folder_path in folder_path_list:
@@ -37,6 +33,10 @@ if __name__ == '__main__':
         path = utils.get_key_from_folder_path(folder_path)
         FileWriter.clean_create_directory(frontend_image_path, path)
 
+    print('storing all keyframes on disk')
+    FileWriter.store_all_keyframes(frontend_image_path, video_dict)
+
+    print('storing keyframe data in mongoDB')
     # mongo_handler.store_all_keyframes(video_dict)
 
     # store results on disk (with bool condition)
