38b7b70c9fd15bf93b02df59cae9d4c40e4b05a8
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-07-23 21:25:34 +0200

    Rearranged arrow functions on top of class

--- server/index.js
+++ server/index.js
@@ -9,6 +9,25 @@ let KeyframeModel = require('./model');
 
 const router = express.Router();
 
+const conceptStringHelper = (concept) => {
+    if (concept === undefined || concept === "" || !concept instanceof String) {
+        return "";
+    }
+    return concept.toLowerCase();
+}
+
+const confidenceStringHelper = (confidence) => {
+    if (confidence === undefined || confidence === "") {
+        return [0, 1];
+    }
+    let conf_split = confidence.split(',')
+
+    if (conf_split.length !== 2) {
+        return [0, 1];
+    }
+    return [conf_split[0], conf_split[1]];
+};
+
 mongoose.connect("mongodb://127.0.0.1:27017/keyframe_small", {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -31,24 +50,6 @@ router.route('/getHelloWorld').get((request, response) => {
     response.send('Hello World');
 });
 
-const conceptStringHelper = (concept) => {
-    if (concept === undefined || concept === "" || !concept instanceof String) {
-        return "";
-    }
-    return concept.toLowerCase();
-}
-
-const confidenceStringHelper = (confidence) => {
-    if (confidence === undefined || confidence === "") {
-        return [0, 1];
-    }
-    let conf_split = confidence.split(',')
-
-    if (conf_split.length !== 2) {
-        return [0, 1];
-    }
-    return [conf_split[0], conf_split[1]];
-};
 
 router.route("/filter").get(function (request, response) {
     try {
@@ -58,11 +59,7 @@ router.route("/filter").get(function (request, response) {
         let confidence = confidenceStringHelper(request.query.confidence);
         let lower_confidence = confidence[0];
         let upper_confidence = confidence[1];
-        // console.log('concept: ' + concept);
-        // console.log('confidence: ' + lower_confidence);
-        // console.log('confidence: ' + upper_confidence);
         let regexString = `^.*${concept}.*$`;
-        //if (req.query.concept === "") {
         filter = {
             'concept_confidence': {$elemMatch: {$elemMatch: {$regex: regexString, $options: 'i'}}}
         }
